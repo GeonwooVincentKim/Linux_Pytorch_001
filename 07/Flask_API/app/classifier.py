3465b71683aa2310bf479ee8c00313d3d09f6802
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-31 20:39:50 +0900

    Created initializer-function of Classifier class.

--- 07/Flask_API/app/classifier.py
+++ 07/Flask_API/app/classifier.py
@@ -20,3 +20,31 @@ def create_network():
     fc_input_dim = net.fc.in_features
     net.fc = nn.Linear(fc_input_dim, 2)
     return net
+
+
+"""
+    Generate 'classifier.py' class.
+"""
+
+
+class Classifier(object):
+    def __init__(self, params):
+        # Create Discrimination-Network.
+        self.net = create_network()
+
+        # Set training-finished parameter.
+        self.net.load_state_dict(params)
+
+        # Set as evaluation-mode.
+        self.net.eval()
+
+        # A function that convert Image as Tensor.
+        self.transformer = transforms.Compose([
+            transforms.CenterCrop(224),
+            transforms.ToTensor()
+        ])
+
+        # Connect classification-ID and classification-Name.
+        self.classes = ["burrito", "taco"]
+
+
