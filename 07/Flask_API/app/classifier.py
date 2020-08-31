35ee4f929d4f0c1880316db60078be985ab1ddec
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-31 20:24:16 +0900

    Successfully generated 'classifier.py.

--- 07/Flask_API/app/classifier.py
+++ 07/Flask_API/app/classifier.py
@@ -1,4 +1,22 @@
 # Classifier.py
 from torch import nn
+from torchvision import transforms, models
 
 
+"""
+    Generate 'Neural-Network' function.
+"""
+
+
+def create_network():
+    # Read resnet18
+    # It doesn't need 'pretrained=True'
+    # because parameter set values
+    # behind of model.
+    net = models.resnet18()
+
+    # Change Last-Layer by change it as
+    # 2-outputs Linear-Layer.
+    fc_input_dim = net.fc.in_features
+    net.fc = nn.Linear(fc_input_dim, 2)
+    return net
